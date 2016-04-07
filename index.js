27edb4d0c022e78c8d0e0477ffb01bd3a83bfb3d
Mike Friedman <friedo@friedo.com>
2016-04-07 11:53:03 -0400

    initial

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var _ = require( 'lodash' );
+var GitHub = require( 'github' );
+
 module.exports = {
     /**
      * The main entry point for the Dexter module
@@ -6,8 +9,27 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var repo    = step.input( 'repo' ).first();
+        var title   = step.input( 'title' ).first();
+        var body    = step.input( 'body' ).first();
+
+        var client  = new GitHub( { version: '3.0.0' } );
+
+        var credentials = dexter.provider( 'github' ).credentials();
+
+        client.authenticate( {
+            type:    'oauth',
+            token:   credentials.access_token
+        } );
+
+        github.issues.create( {
+            repo:     repo,
+            title:    title,
+            body:     body
+        }, function( err, res ) {
+            if ( err ) return this.fail( err );
+            return this.complete( res );
+        }.bind( this ) );
+
     }
 };
